9e684b876dd09e1471cb0bf6c12f13b5e8aeb096
Dave Hausler <dave.hausler@gmail.com>
2022-09-13 09:28:43 -0400

    change cover page

---
+++ assets/DrawingPreview.3d8475cc.js
@@ -0,0 +1,1 @@
+import"./index.5a57e557.js";import{ah as r}from"./index.5a57e557.js";export{r as default};
--- assets/DrawingPreview.8732646b.js
+++
@@ -1,1 +0,0 @@
-import"./index.3cdae54c.js";import{ah as r}from"./index.3cdae54c.js";export{r as default};

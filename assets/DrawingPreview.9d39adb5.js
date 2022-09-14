a0c0870d456cd5c04b9a17073e43e628d0b78a8b
Dave Hausler <dave.hausler@gmail.com>
2022-09-14 08:59:21 -0400

    add navigation links to summary pages

---
+++ assets/DrawingPreview.9d39adb5.js
@@ -0,0 +1,1 @@
+import"./index.4abd541c.js";import{ah as r}from"./index.4abd541c.js";export{r as default};
--- assets/DrawingPreview.d27df49f.js
+++
@@ -1,1 +0,0 @@
-import"./index.4638f4da.js";import{ah as r}from"./index.4638f4da.js";export{r as default};

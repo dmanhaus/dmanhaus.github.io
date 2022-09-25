07cf34245d915f39dce39d082eb241d21adfff55
Dave Hausler <dave.hausler@gmail.com>
2022-09-25 11:11:38 -0400

    add slides rome, cruise departure

---
+++ assets/DrawingPreview.aa4bc530.js
@@ -0,0 +1,1 @@
+import"./index.48403afe.js";import{ah as r}from"./index.48403afe.js";export{r as default};
--- assets/DrawingPreview.d5a674ff.js
+++
@@ -1,1 +0,0 @@
-import"./index.3d5f80a3.js";import{ah as r}from"./index.3d5f80a3.js";export{r as default};

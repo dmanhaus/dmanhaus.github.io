ce5dc85d1d04806a96dc5097cdcfa0b429cc2b43
Dave Hausler <dave.hausler@gmail.com>
2022-09-27 22:30:16 -0400

    add spanish steps

---
+++ assets/DrawingControls.95d7806e.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.dac18db3.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.dac18db3.js";import"./index.112fdac9.js";export{t as default};
--- assets/DrawingPreview.a44659e5.js
+++
@@ -1,1 +0,0 @@
-import"./index.e3578a73.js";import{ah as r}from"./index.e3578a73.js";export{r as default};
